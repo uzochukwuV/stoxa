bc4e6e6444185f75fbfc0ee3c6f202fe0eebcdb7
uzochukwuV <vic.ezealor@gmail.com>
2023-10-15 02:23:37 +0200

    made adjustment to piplock file

--- build.sh
+++ build.sh
@@ -2,7 +2,12 @@
 
 # Build the project
 echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+
+python3.9 -m pip install pipenv
+
+python3.9 -m pipenv shell
+
+python3.9 -m pipenv sync
 
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
