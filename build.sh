6471be92456d5732c21830b7640edf58da494dd7
uzochukwuV <vic.ezealor@gmail.com>
2023-10-15 02:45:54 +0200

    no py version included

--- build.sh
+++ build.sh
@@ -6,24 +6,24 @@ echo "Building the project..."
 python --version
 
 echo "installing pipenv"
-python3.11 -m pip install pipenv
+python -m pip install pipenv
 
 echo "shelling pip now"
 
-python3.11 -m pipenv shell
+python -m pipenv shell
 
 echo "pipenv syncing -----------"
 
-python3.11 -m pipenv sync
+python -m pipenv sync
 
 echo "pipenv installing django ----------"
 
 
-python3.11 -m pipenv install django
+python -m pipenv install django
 
 echo "Make Migration..."
-python3.11 manage.py makemigrations --noinput
-python3.11 manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.11 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
