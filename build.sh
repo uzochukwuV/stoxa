b103502a69dae565dd3840e9a28224fa3523422c
uzochukwuV <vic.ezealor@gmail.com>
2023-10-15 02:40:47 +0200

    changed python version in vercel.json and build.sh to 3.11

--- build.sh
+++ build.sh
@@ -3,24 +3,27 @@
 # Build the project
 echo "Building the project..."
 
+python --version
 
 echo "installing pipenv"
-python3.9 -m pip install pipenv
+python3.11 -m pip install pipenv
 
 echo "shelling pip now"
 
-python3.9 -m pipenv shell
+python3.11 -m pipenv shell
 
 echo "pipenv syncing -----------"
 
-python3.9 -m pipenv sync
+python3.11 -m pipenv sync
 
 echo "pipenv installing django ----------"
-python3.9 -m pipenv install django
+
+
+python3.11 -m pipenv install django
 
 echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3.11 manage.py makemigrations --noinput
+python3.11 manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
+python3.11 manage.py collectstatic --noinput --clear
